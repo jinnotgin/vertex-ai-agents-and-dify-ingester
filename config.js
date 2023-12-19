46c6d66e5d76569b2d23ad0c8ce75d0f9114f489
Jin Lin <me@thelinjin.com>
2023-12-19 16:20:33 +0800

    Updated configs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const crawlTargets = {
-	// all: {
+	// everything: {
 	// 	type: "all",
 	// },
 	hrCorner: {
--- secrets.example.js
+++ secrets.example.js
@@ -1,15 +1,12 @@
-export const ATLASSIAN_CLOUD_URL = "";
-export const ATLASSIAN_ACCOUNT_USERNAME = "";
-export const ATLASSIAN_ACCOUNT_API_TOKEN = "";
+export const ATLASSIAN_CLOUD_URL = "https://<something>.atlassian.net";
+export const ATLASSIAN_ACCOUNT_USERNAME = "something@something.com";
+export const ATLASSIAN_ACCOUNT_API_TOKEN = "<API TOKEN FOR YOUR ACCOUNT>";
 
-export const GCP_PROJECT_ID = "";
-export const GCS_BUCKET_NAME = "";
+export const GCP_PROJECT_ID = "<GOOGLE CLOUD PROJECT ID>";
+export const GCS_BUCKET_NAME = "<GOOGLE CLOUD STORAGE BUCKET NAME>";
 export const VERTEX_DATA_STORES = {
-	// ... (in the sample config.js given, there will be 3 crawlTargets keys: "all", "hrCorner" and "officeReopening")
-	all: "<VERTEX SEARCH DATA STORE ID>",
+	// ... (in the sample config.js given, there will be 3 crawlTargets tasks: "everything", "hrCorner" and "officeReopening")
+	everything: "<VERTEX SEARCH DATA STORE ID>",
 	hrCorner: "<VERTEX SEARCH DATA STORE ID>",
 	officeReopening: "<VERTEX SEARCH DATA STORE ID>",
-	// feel free to add more to match your config.js crawlTargets structure
-	CRAWL_TARGETS_KEY1: "<VERTEX SEARCH DATA STORE ID>",
-	CRAWL_TARGETS_KEY2: "<VERTEX SEARCH DATA STORE ID>",
 };
