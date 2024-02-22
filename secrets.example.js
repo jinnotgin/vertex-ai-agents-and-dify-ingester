2136bf6917f5375e35d198bbb21d6277df6a8f3e
Jin <me@thelinjin.com>
2024-02-22 21:42:55 +0800

    Update secrets.example.js

--- secrets.example.js
+++ secrets.example.js
@@ -2,11 +2,17 @@ export const ATLASSIAN_CLOUD_URL = "https://<something>.atlassian.net";
 export const ATLASSIAN_ACCOUNT_USERNAME = "something@something.com";
 export const ATLASSIAN_ACCOUNT_API_TOKEN = "<API TOKEN FOR YOUR ACCOUNT>";
 
-export const GCP_PROJECT_ID = "<GOOGLE CLOUD PROJECT ID>";
-export const GCS_BUCKET_NAME = "<GOOGLE CLOUD STORAGE BUCKET NAME>";
+export const ZEPHYR_SQUAD_ACCESS_KEY = "<ZEPHYR_SQUAD_ACCESS_KEY>";
+export const ZEPHYR_SQUAD_SECRET_KEY = "<ZEPHYR_SQUAD_SECRET_KEY>";
+export const ZEPHYR_SQUAD_ATLASSIAN_ACCOUNT_ID =
+	"<ZEPHYR_SQUAD_ATLASSIAN_ACCOUNT_ID>";
+
+export const GCP_PROJECT_ID = "<GOOGLE_CLOUD_PROJECT_ID>";
+export const GCS_BUCKET_NAME = "<GOOGLE_CLOUD_STORAGE_BUCKET_NAME>";
 export const VERTEX_DATA_STORES = {
-	// ... (in the sample config.js given, there will be 3 crawlTargets tasks: "everything", "hrCorner" and "officeReopening")
-	everything: "<VERTEX SEARCH DATA STORE ID>",
-	hrCorner: "<VERTEX SEARCH DATA STORE ID>",
-	officeReopening: "<VERTEX SEARCH DATA STORE ID>",
+	// ... (in the sample config.js given, there will be 4 crawlTargets tasks: "everything", "hrCorner", "officeReopening" and "userSupport")
+	everything: "<VERTEX_SEARCH DATA_STORE_ID>",
+	hrCorner: "<VERTEX_SEARCH_DATA_STORE_ID>",
+	officeReopening: "<VERTEX_SEARCH_DATA_STORE_ID>",
+	userSupport: "<VERTEX_SEARCH_DATA_STORE_ID>",
 };
